5c1312bb2688d7038a467646fbeea5a28a5b9de3
trungi <t.trungel@gmail.com>
2014-12-06 15:24:27 +0100

    Fixed a bug in hand raised.

--- lib/recognition/hand_raised.m
+++ lib/recognition/hand_raised.m
@@ -1,13 +1,13 @@
 function raised_hand = hand_raised( seg_img )
     %Checks whether hand is raised
-    
+    % figure; imshow(seg_img);
     height = size(seg_img, 1);
     width = size(seg_img, 2);
-
-     if width/height > 57
-        raised_hand = 1
+    
+     if width/height > 0.57
+        raised_hand = 0
      else
-         raised_hand = 0
+         raised_hand = 1
      end
 end
 
